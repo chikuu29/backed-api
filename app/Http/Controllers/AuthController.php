6beeddba81e1360ed7e0f271adda28f7c763a823
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-03-18 09:50:05 +0530

    done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -198,7 +198,7 @@ class AuthController extends Controller
 
                 // Generate a random token
                 $token = Str::random(60);
-                $expiration = Carbon::now()->addHours(12)->timestamp;
+                $expiration = Carbon::now()->addHours(1)->timestamp;
                 $key = env('JWT_SECRET');  // Secret key from .env or configuration
                 $payload = [
                     'email' => $userdata->auth_email,
@@ -259,6 +259,7 @@ class AuthController extends Controller
     {
 
         $authorizationHeader = $request->header('Authorization');
+        // echo $authorizationHeader;
         if ($authorizationHeader) {
             // list($bearer, $token) = explode(' ', $authHeader, 2);
             // echo $token;
@@ -313,52 +314,53 @@ class AuthController extends Controller
         $key = '1E99412323A4ED2WAYWALASECRET_KEY';
         return openssl_decrypt($data, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
     }
+}
 
 
-    function cryptoJsAesDecrypt($jsonString)
-    {
-        $passphrase = '1E99412323A4ED2WAYWALASECRET_KEY';
-        $jsondata = json_decode($jsonString, true);
-        try {
-            $salt = hex2bin($jsondata["s"]);
-            $iv  = hex2bin($jsondata["iv"]);
-        } catch (Exception $e) {
-            return null;
-        }
-        $ct = base64_decode($jsondata["ct"]);
-        $concatedPassphrase = $passphrase . $salt;
-        $md5 = array();
-        $md5[0] = md5($concatedPassphrase, true);
-        $result = $md5[0];
-        for ($i = 1; $i < 3; $i++) {
-            $md5[$i] = md5($md5[$i - 1] . $concatedPassphrase, true);
-            $result .= $md5[$i];
-        }
-        $key = substr($result, 0, 32);
-        $data = openssl_decrypt($ct, 'aes-256-cbc', $key, true, $iv);
-        return json_decode($data, true);
+
+function cryptoJsAesDecrypt($jsonString)
+{
+    $passphrase = '1E99412323A4ED2WAYWALASECRET_KEY';
+    $jsondata = json_decode($jsonString, true);
+    try {
+        $salt = hex2bin($jsondata["s"]);
+        $iv  = hex2bin($jsondata["iv"]);
+    } catch (Exception $e) {
+        return null;
     }
-    /**
-     * Encrypt value to a cryptojs compatiable json encoding string
-     *
-     * @param mixed $passphrase
-     * @param mixed $value
-     * @return string
-     */
-    function cryptoJsAesEncrypt($value)
-    {
-        $passphrase = '1E99412323A4ED2WAYWALASECRET_KEY';
-        $salt = openssl_random_pseudo_bytes(8);
-        $salted = '';
-        $dx = '';
-        while (strlen($salted) < 48) {
-            $dx = md5($dx . $passphrase . $salt, true);
-            $salted .= $dx;
-        }
-        $key = substr($salted, 0, 32);
-        $iv  = substr($salted, 32, 16);
-        $encrypted_data = openssl_encrypt(json_encode($value), 'aes-256-cbc', $key, true, $iv);
-        $data = array("ct" => base64_encode($encrypted_data), "iv" => bin2hex($iv), "s" => bin2hex($salt));
-        return json_encode($data);
+    $ct = base64_decode($jsondata["ct"]);
+    $concatedPassphrase = $passphrase . $salt;
+    $md5 = array();
+    $md5[0] = md5($concatedPassphrase, true);
+    $result = $md5[0];
+    for ($i = 1; $i < 3; $i++) {
+        $md5[$i] = md5($md5[$i - 1] . $concatedPassphrase, true);
+        $result .= $md5[$i];
     }
+    $key = substr($result, 0, 32);
+    $data = openssl_decrypt($ct, 'aes-256-cbc', $key, true, $iv);
+    return json_decode($data, true);
+}
+/**
+ * Encrypt value to a cryptojs compatiable json encoding string
+ *
+ * @param mixed $passphrase
+ * @param mixed $value
+ * @return string
+ */
+function cryptoJsAesEncrypt($value)
+{
+    $passphrase = '1E99412323A4ED2WAYWALASECRET_KEY';
+    $salt = openssl_random_pseudo_bytes(8);
+    $salted = '';
+    $dx = '';
+    while (strlen($salted) < 48) {
+        $dx = md5($dx . $passphrase . $salt, true);
+        $salted .= $dx;
+    }
+    $key = substr($salted, 0, 32);
+    $iv  = substr($salted, 32, 16);
+    $encrypted_data = openssl_encrypt(json_encode($value), 'aes-256-cbc', $key, true, $iv);
+    $data = array("ct" => base64_encode($encrypted_data), "iv" => bin2hex($iv), "s" => bin2hex($salt));
+    return json_encode($data);
 }
