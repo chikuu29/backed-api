d4a5c78e17c934a33946bade342a48dd5368ffcd
Gyana1995 <100307598+Gyana1995@users.noreply.github.com>
2024-03-31 16:56:14 +0530

    gyana
    ok

--- routes/web.php
+++ routes/web.php
@@ -95,13 +95,14 @@ $router->group(['middleware' => ['App\Http\Middleware\JwtMiddleware']], function
     $router->post("/byCastgetOnlinedata", "filterController@byCastgetOnlinedata");
     $router->post("/byCastgetSpotlightdata", "filterController@byCastgetSpotlightdata");
     $router->post("/byCastRecentlyJoinedMatches", "filterController@byCastRecentlyJoinedMatches");
-
+    $router->post("/byCastmatchesforindivisual", "filterController@byCastmatchesforindivisual");
 
 
     $router->post("/byOtherCastpremimusMatches", "filterController@byOtherCastpremimusMatches");
     $router->post("/byOtherCastgetOnlinedata", "filterController@byOtherCastgetOnlinedata");
     $router->post("/byOtherCastgetSpotlightdata", "filterController@byOtherCastgetSpotlightdata");
     $router->post("/byOtherCastRecentlyJoinedMatches", "filterController@byOtherCastRecentlyJoinedMatches");
+    $router->post("/byOtherCastmatchesforindivisual", "filterController@byOtherCastmatchesforindivisual");
 });
 
 $router->get("/setting", "AppController@settings");
@@ -115,6 +116,3 @@ $router->post("/getAllData", "memberController@getAllData");
 $router->post("/aboutus", "addItemControler@aboutus");
 $router->post("/unsecuredFatchquary", "dynamic_Crud_controller@unsecuredFatchquary");
 $router->post("/feedback", "AppController@feedback");
-$router->post("/byCastmatchesforindivisual", "filterController@byCastmatchesforindivisual");
-$router->post("/byOtherCastmatchesforindivisual", "filterController@byOtherCastmatchesforindivisual");
-
