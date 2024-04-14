f4a1b7b619ff783e919c58d3d43f87b1eca720ed
Gyana1995 <100307598+Gyana1995@users.noreply.github.com>
2024-04-14 22:35:13 +0530

    ok
    gyana

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -92,7 +92,6 @@ class registerController extends Controller
         $newdata = $input['newdata'] == '' || $input['newdata'] == null  ? '' : $input['newdata'];
         $tablename = $input['tablename'] == '' || $input['tablename'] == null  ? '' : $input['tablename'];
         $coulemnname = $input['coulemnname'] == '' || $input['coulemnname'] == null  ? '' : $input['coulemnname'];
-
         DB::table($tablename)->where($coulemnname,$olddata)->update([
             $coulemnname => $newdata
         ]);
