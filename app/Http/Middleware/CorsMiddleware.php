dad3be2e697d3fb06dcdfc23dbcb06745050a6b6
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-06-11 21:18:06 +0530

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,49 +8,36 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        
-        // // Add CORS headers
-        // $response->headers->set('Access-Control-Allow-Origin', '*');
-        // $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        // $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        
-        // return $response;
         $allowedOrigins = [
             'http://localhost:4200',
             'https://admin.choicemarriage.com'
         ];
-    
+
         $origin = $request->header('Origin');
+
+        // Initializing headers array
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
         if (in_array($origin, $allowedOrigins)) {
             $headers['Access-Control-Allow-Origin'] = $origin;
-        }else{
-             return $response;
-            
         }
-            // var_dumb($request);
-            $headers = [
-                'Access-Control-Allow-Origin'      => $origin,
-              'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-              'Access-Control-Allow-Credentials' => 'true',
-              'Access-Control-Max-Age'           => '86400',
-              'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
-    
-          ];
 
-      
+        // If the request method is OPTIONS, we respond with allowed headers
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
 
-       if ($request->isMethod('OPTIONS'))
-       {
-           return response()->json('{"method":"OPTIONS"}', 200, $headers);
-       }
+        // Continue processing the request and add headers to the response
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
 
-       $response = $next($request);
-       foreach($headers as $key => $value)
-       {
-           $response->header($key, $value);
-       }
-
-       return $response;
+        return $response;
     }
 }
